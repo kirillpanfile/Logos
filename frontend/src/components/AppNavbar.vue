cae3215c83cb03d7a2c16960af9905a4994d642c
RasslaBones <94486831+RasslaBones@users.noreply.github.com>
2022-07-06 20:28:17 +0300

    Almost finished nav-bar

--- frontend/src/components/AppNavbar.vue
+++ frontend/src/components/AppNavbar.vue
@@ -1,11 +1,50 @@
 <template>
-  <div>navbar</div>
+  <div class="header">
+    <div class="header__wrapper">
+      <div class="icon-menu" @click="isBurgherMenuOpen = !isBurgherMenuOpen">
+        <span></span>
+        <div class="icon-menu-text">МЕНЮ</div>
+      </div>
+      <div class="burgher-menu" v-if="isBurgherMenuOpen" @click="isBurgherMenuOpen = false">132</div>
+      <div class="header__logo">LOGOS</div>
+      <div class="header__input">
+        <img src="@/assets/images/Icons/Navbar/Location.svg" />
+        <input type="text">
+        <img src="@/assets/images/Icons/Navbar/Search.svg"/>
+      </div>
+      <div class="header__call call">
+        <div class="call__icon" @click="isCallOpen = !isCallOpen">
+          <img src="@/assets/images/Icons/Navbar/Calling.png" />
+          <div class="call__modal" v-if="isCallOpen">
+            <div class="call__modal-title">Позвоните нам!</div>
+            <div class="call__modal-number">+7 (917) 510-57-59</div>
+          </div>
+        </div>
+        
+        <div class="call__desc">
+          <div class="call__title">Контакты:</div>
+          <div class="call__number">+7 (917) 510-57-59</div>
+        </div>
+      </div>
+      <div class="header__button">
+        <base-button
+          type="nav"
+          color="green"
+          text="Корзина"
+        ></base-button>
+      </div>
+    </div>   
+  </div>
 </template>
 
 <script>
 export default {
   name: "AppNavbar",
+  data(){
+    return{
+      isCallOpen: false,
+      isBurgherMenuOpen: false
+    }
+  }
 };
 </script>
-
-<style></style>
--- frontend/src/components/UI/BaseButton.vue
+++ frontend/src/components/UI/BaseButton.vue
@@ -1,11 +1,53 @@
 <template>
-  <div>Button</div>
+  <div class="button base-button" :class="color + '-base'">
+  <div class="button-nav-mobile__wrapper" v-if="type == 'nav'">
+    <img src="@/assets/images/Icons/Product/cart.png" />
+    <div class="button-nav-mobile__title">{{text}}</div>
+  </div>
+    <div :class="classType">
+      <div class="button__card-title" v-if="type == 'card'">{{text}}</div>
+      <div class="button__title" v-else-if="type != 'order' && type != 'counter'">{{text}}</div>
+      <div class="button__title-borderless" v-else-if="type == 'order'">{{text}}</div>
+      <img src="@/assets/images/Icons/Product/shopping-bag.png" v-if="type == 'prod'" />
+      <img src="@/assets/images/Icons/Product/cart.png" v-else-if="type == 'card'" />
+      <img src="@/assets/images/Icons/Product/minus.png" v-else-if="type == 'counter' && text == '-'" />
+      <img src="@/assets/images/Icons/Product/plus.png" v-else-if="type == 'counter' && text == '+'" />
+      <div class="button__image" v-else-if="type == 'nav'">4</div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: "BaseButton",
-};
-</script>
+  props:{
+    type:{
+      type: String,
+      default: 'nav',
+      validator(val){
+        return ['nav','prod','card','order','counter'].includes(val)
+      }
+    },
+    text:{
+      type: String,
+      default: 'Корзина'
+    },
+    color:{
+      type: String,
+      default: 'green',
+      validator(val){
+        return ['green', 'black'].includes(val)
+      }
+    }
+  },
+  computed: {
+    classType(){
+      let className = `button__wrapper-${this.type}`
+      if(this.type == 'nav')
+        return `${className} button-${this.type}-hide `
 
-<style></style>
+      return className
+      }
+  }
+}
+</script>
