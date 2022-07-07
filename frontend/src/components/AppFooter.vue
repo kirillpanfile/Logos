aaf0a0186080b70a9ad81fdc0ff5343cfc57f1e5
RasslaBones <94486831+RasslaBones@users.noreply.github.com>
2022-07-07 21:30:17 +0300

    Finished footer

--- frontend/src/components/AppFooter.vue
+++ frontend/src/components/AppFooter.vue
@@ -1,5 +1,30 @@
 <template>
-  <div>Footer</div>
+<div class="footer">
+  <div class="footer__wrapper">
+    <div class="footer__button">
+      <img src="@/assets/images/Icons/Footer/Arrow.svg">
+    </div>
+    <div class="footer__info">
+      <h1 class="footer__logo">LOGOS</h1>
+      <div class="footer__small"><i>
+        © ООО СК «АПШЕРОН» 
+        Все права защищены. 2010-2020
+      </i></div>
+      <div class="footer__links">
+        <a href="#"><i>Пользовательское соглашение</i></a>
+        <a href="#"><i>Карта сайта</i></a>
+        <a href="#"><i>Политика конфиденциальности</i></a>
+      </div>
+    </div>
+    <div class="footer__options">
+      <a href="#"><i>О ресторане</i></a>
+      <a href="#"><i>Условия доставки</i></a>
+      <a href="#"><i>Возврат товара</i></a>
+      <a href="#"><i>Акции</i></a>
+    </div>
+  </div>
+</div>
+  
 </template>
 
 <script>
